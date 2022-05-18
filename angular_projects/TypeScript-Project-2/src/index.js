8346ed4f8c2f0da0d1578011d1c28b4d36103e2f
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 02:31:47 +0530

    april19 task done

--- angular_projects/TypeScript-Project-2/src/index.js
+++ angular_projects/TypeScript-Project-2/src/index.js
@@ -1,13 +1,13 @@
 //let username:string = prompt("Enter user name");
-var username = prompt("Enter user name");
+var username = prompt("Enter user name"); //in javascript prompt have ok and cancel button
 function DisplayName() {
     if (username == null) {
-        document.getElementById("container").innerHTML = "Please enter user name";
+        document.getElementById("container").innerHTML = "Please enter user name"; //cancel button
     }
     else if (username == "") {
-        document.getElementById("container").innerHTML = "Name can't be Empty";
+        document.getElementById("container").innerHTML = "Name can't be Empty"; //ok button
     }
     else {
-        document.getElementById("container").innerHTML = "Hello ! " + username;
+        document.getElementById("container").innerHTML = "Hello ! " + username; //enter character
     }
 }
--- angular_projects/TypeScript-Project-2/src/index.ts
+++ angular_projects/TypeScript-Project-2/src/index.ts
@@ -1,15 +1,15 @@
 //let username:string = prompt("Enter user name");
 
-let username:string|null = prompt("Enter user name");
+let username:string|null = prompt("Enter user name");//in javascript prompt have ok and cancel button
 
 function DisplayName(){
 
     if(username==null){
-        document.getElementById("container").innerHTML = "Please enter user name";
+        document.getElementById("container").innerHTML = "Please enter user name";//cancel button
     } else if(username=="") {
-        document.getElementById("container").innerHTML = "Name can't be Empty"
+        document.getElementById("container").innerHTML = "Name can't be Empty"//ok button
     } else {
-        document.getElementById("container").innerHTML = "Hello ! " + username;
+        document.getElementById("container").innerHTML = "Hello ! " + username;//enter character
     }
     
 }
