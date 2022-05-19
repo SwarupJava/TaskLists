8188e5c62ffea7f035db35596e28d6f80ad51c6c
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 16:43:52 +0530

    april20 task

---
+++ angular_projects/TypeScript-Project-14/src/index.ts
@@ -0,0 +1,14 @@
+let productMap:any = new Map();
+productMap.set(1, "SAMSUNG TV");
+productMap.set("Watch", "Fastrack Watch");
+
+console.log(productMap.get("Watch"));
+
+
+
+
+
+//npm init -y
+//tsc index.ts
+//node index.js
+//find the value from key
