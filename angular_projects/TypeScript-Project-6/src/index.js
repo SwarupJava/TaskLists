87f573a4d1c13f3c5c7635d887c79f60a2d2a40c
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 14:48:59 +0530

    april20 task

---
+++ angular_projects/TypeScript-Project-6/src/index.js
@@ -0,0 +1,7 @@
+var categories = ["Electronics", "Footwear", "Fashion"];
+for (var property in categories) {
+    console.log("[".concat(property, "] - ").concat(categories[property]));
+}
+//npm init -y
+//tsc index.ts
+//node index.js
---
+++ angular_projects/TypeScript-Project-6/src/index.ts
@@ -0,0 +1,12 @@
+let categories:string[] = ["Electronics", "Footwear", "Fashion"];
+for(var property in categories){
+    console.log(`[${property}] - ${categories[property]}`);
+}
+
+
+
+
+
+//npm init -y
+//tsc index.ts
+//node index.js
