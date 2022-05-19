51198d35d70e406296838ac2ca88afc944b47531
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 15:59:20 +0530

    april20 task

---
+++ angular_projects/TypeScript-Project-11/src/index.js
@@ -0,0 +1,6 @@
+var products = [
+    { Id: 1, Name: "TV", Price: 45600.44 },
+    { Id: 2, Name: "Mobile", Price: 5600.33 }
+];
+console.log("Mobile Price : ".concat(products[1].Price));
+//npm init -y
---
+++ angular_projects/TypeScript-Project-11/src/index.ts
@@ -0,0 +1,13 @@
+let products:any[] = [
+    {Id:1, Name:"TV", Price:45600.44},
+    {Id:2, Name: "Mobile", Price:5600.33}
+]
+
+console.log(`Mobile Price : ${products[1].Price}`);
+
+
+
+
+
+
+//npm init -y
