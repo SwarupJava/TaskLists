0e544eb96f03a258931da849a24420c2a1230786
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 15:31:11 +0530

    april20 task

---
+++ angular_projects/TypeScript-Project-9/src/index.js
@@ -0,0 +1,6 @@
+//let values:string[]|number[] = [10, "A"];//union of types can not be initialized
+var values = [];
+values[0] = "A"; //union of types can be assigned
+values[1] = 10; //union of types can be assigned
+//npm init -y
+//check union of types
---
+++ angular_projects/TypeScript-Project-9/src/index.ts
@@ -0,0 +1,15 @@
+//let values:string[]|number[] = [10, "A"];//union of types can not be initialized
+
+let values:string[]|number[] = [];
+values[0] = "A";//union of types can be assigned
+values[1] = 10;//union of types can be assigned
+
+
+
+
+
+
+
+
+//npm init -y
+//check union of types
