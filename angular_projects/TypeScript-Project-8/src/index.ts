ed55a400dfe1724d3f448ca7547f06f317c940a0
SwarupKP127 <swarupkp12@gmail.com>
2022-05-19 15:16:29 +0530

    april20 task

---
+++ angular_projects/TypeScript-Project-8/src/index.ts
@@ -0,0 +1,17 @@
+let sales:number[] = [40000.33, 13000.44, 56000.33, 23000.33];
+
+let result:number = sales.find(function(value){
+    return value > 20000;
+})
+
+console.log(result);
+
+
+
+
+
+
+
+//npm init -y
+//tsc index.ts
+//node index.js
