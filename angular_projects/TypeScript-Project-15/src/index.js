1acf7e031be85951891e5d1abd86b8188aaf3ec9
SwarupKP127 <swarupkp12@gmail.com>
2022-05-22 05:57:43 +0530

    april21 task

---
+++ angular_projects/TypeScript-Project-15/src/index.js
@@ -0,0 +1,9 @@
+var Name = "Samsung TV";
+var Price = 45000.44;
+var Stock = true;
+var Mfd = new Date("2022-02-10");
+console.log(Mfd.toLocaleDateString());
+//npm init -y
+//tsc index.ts
+//node index.js
+//for time
---
+++ angular_projects/TypeScript-Project-15/src/index.ts
@@ -0,0 +1,18 @@
+let Name:string = "Samsung TV";
+let Price:number = 45000.44;
+let Stock:boolean = true;
+let Mfd:any = new Date("2022-02-10");
+
+console.log(Mfd.toLocaleDateString());
+
+
+
+
+
+
+
+
+//npm init -y
+//tsc index.ts
+//node index.js
+//for time
