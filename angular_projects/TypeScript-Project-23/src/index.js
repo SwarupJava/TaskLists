23e0730cf385a63fd6c90dd9fe51e14b25bf2601
SwarupKP127 <swarupkp12@gmail.com>
2022-05-31 12:25:18 +0530

    april22 task

---
+++ angular_projects/TypeScript-Project-23/src/index.js
@@ -0,0 +1,11 @@
+var TV = "Samsung TV";
+var Mobile = "LG mobile";
+var Price = 45000.44;
+var tv = {
+    Name: "Samsung TV",
+    Price: 46000.44
+};
+//npm init -y
+//Object is used to keep all related data and logic together
+//tsc index.ts
+//node index.js
---
+++ angular_projects/TypeScript-Project-23/src/index.ts
@@ -0,0 +1,20 @@
+let TV:string = "Samsung TV";
+let Mobile:string = "LG mobile";
+let Price:number = 45000.44;
+
+let tv = {
+   Name : "Samsung TV",
+   Price : 46000.44
+}
+
+
+
+
+
+
+
+
+//npm init -y
+//Object is used to keep all related data and logic together
+//tsc index.ts
+//node index.js
