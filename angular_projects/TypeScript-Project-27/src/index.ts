dd3a53cf5a175ca4b9443e1b444355130a34e3b9
Swarup189 <swarupkp12@gmail.com>
2023-06-10 19:18:19 +0530

    april25 code

--- angular_projects/TypeScript-Project-27/src/index.ts
+++ angular_projects/TypeScript-Project-27/src/index.ts
@@ -20,10 +20,38 @@ class Product
 let obj = new Product();
 obj.ProductName = productname;  // setter
 if(obj.ProductName){
-    document.write(`Product Name : ${obj.ProductName}`);
+    document.write(`Product Name : ${obj.ProductName}`);//getter
 }
+
+
+
+
+
 
 
 
 //npm init -y
 //tsc index.ts
+
+
+
+//D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-27>npm init -y
+//D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-27\src>tsc index.ts
+/*
+D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-27\src>tsc index.ts
+index.ts:8:8 - error TS1056: Accessors are only available when targeting ECMAScript 5 and higher.
+
+8    get ProductName(){
+         ~~~~~~~~~~~
+
+index.ts:11:8 - error TS1056: Accessors are only available when targeting ECMAScript 5 and higher.
+
+11    set ProductName(newValue) {
+          ~~~~~~~~~~~
+
+
+Found 2 errors in the same file, starting at: index.ts:8
+*/
+//no worry
+//you can continue
+//go to index.html
--- angular_projects/TypeScript-Project-28/src/index.ts
+++ angular_projects/TypeScript-Project-28/src/index.ts
@@ -12,5 +12,30 @@ class Product{
 let obj = new Product();
 obj.Rate;//accessor
 obj.Rating.Rate;// without accessor
+console.log(`Product Name : ${obj.Rating.Rate}`);
 
+
+
+
+
+//accessor
+//with out accessor
+
+//npmn init -y
 //tsc index.ts
+//node index.js
+
+//D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-28>npm init -y
+/*
+D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-28\src>tsc index.ts
+index.ts:8:9 - error TS1056: Accessors are only available when targeting ECMAScript 5 and higher.
+
+8     get Rate(){
+          ~~~~
+
+
+Found 1 error in index.ts:8
+ */
+//no worry
+//you can continue
+//D:\angularsudhakar\code\all_projects\angular_projects\TypeScript-Project-28\src>node index.js
